24f809edefa220b8c89e15fd2ebba1e761047fed
al1ensuu <462993145@qq.com>
2016-07-27 10:23:36 +0100

    Try to fix travis build benchmark
    1st try.
    
    Signed-off-by: al1ensuu <462993145@qq.com>

--- llama_cpp/benchmark/install_benchmark.sh
+++ llama_cpp/benchmark/install_benchmark.sh
@@ -7,7 +7,7 @@ cd $BASEDIR/../../../
 if [ ! -f "benchmark_llama_cpp/src/libbenchmark.a" ]; then
 	mkdir -p benchmark_llama_cpp
 	cd benchmark_llama_cpp
-	cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true -DBENCHMARK_ENABLE_TESTING=false ../Project-Llama/llama_cpp/benchmark/google_benchmark
+	cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true -DBENCHMARK_ENABLE_TESTING=false -DCMAKE_CXX_FLAGS="-std=c++11" ../Project-Llama/llama_cpp/benchmark/google_benchmark
 	make
 	echo "Google benchmark library install complete."
 else
