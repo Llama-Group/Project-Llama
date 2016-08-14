715d8b7b93c7dc92f66b47bec489d591ec6f46d5
BlueCocoa <0xbbc@0xbbc.com>
2016-08-14 22:50:20 +0800

    Add unit test for Utility/Functor.h
    Add unit test for Utility/Functor.h
    
    Signed-off-by: 0xBBC <0xbbc@0xbbc.com>

---
+++ llama_cpp/test/Llama_UnitTest/UtilityFunctorTest.cpp
@@ -0,0 +1,39 @@
+
+//
+//  Copyright © 2016 Project Llama. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#include <gtest/gtest.h>
+#include <string.h>
+#include <Utility/Functor.h>
+
+#include <string>
+
+using std::string;
+
+struct FunctorTest : public ::testing::Test {
+    Functor functor_int = Functor([]() -> int { return 1; });
+    Functor functor_obj = Functor([]() -> string { return "ok"; });
+};
+
+// Return Value
+TEST_F(FunctorTest, ReturnValue) {
+    int result = functor_int.as<int>()();
+    EXPECT_EQ(result, 1);
+
+    const char * return_value_ptr = functor_obj.as<string>()().c_str();
+    result = strcmp(return_value_ptr, "ok");
+    EXPECT_EQ(result, 0);
+}
