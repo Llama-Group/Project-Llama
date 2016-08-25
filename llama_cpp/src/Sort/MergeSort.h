8491da030171fb995034b982c6ab31e542783525
孙宏量 <Sunbelife@foxmail.com>
2016-08-25 18:47:04 +0800

    Space
    4 Space.
    
    Signed-off-by: Sunbelife <Sunbelife@foxmail.com>

--- llama_cpp/src/Sort/MergeSort.h
+++ llama_cpp/src/Sort/MergeSort.h
@@ -27,53 +27,50 @@
 namespace llama {
 template <class T> class MergeSort : public Sort<T> {
  public:
-    MergeSort() {}
-    explicit MergeSort(std::vector<T> *inputArray) {
-    this->performSort(inputArray);
-    }
+        MergeSort() {}
+        explicit MergeSort(std::vector<T> *inputArray) { this->performSort(inputArray); }
 
-    void Merge(std::vector<SortObject<T>> *arr, int low, int mid, int high) {
-      int i = low;
-      int j = mid + 1;
-      int k = 0;
+        void Merge(std::vector<SortObject<T>> *arr, int low, int mid, int high) {
+            int i = low;
+            int j = mid + 1;
+            int k = 0;
+            std::vector<SortObject<T>> tmp(high - low + 1);
 
-    std::vector<SortObject<T>> tmp(high - low + 1);
+            while (i <= mid && j <= high) {
+                if (arr->at(i) <= arr->at(j))
+                    std::swap(tmp.at(k++), arr->at(i++));
+                else
+                    std::swap(tmp.at(k++), arr->at(j++));
+            }
 
-    while (i <= mid && j <= high) {
-      if (arr->at(i) <= arr->at(j))
-        std::swap(tmp.at(k++), arr->at(i++));
-      else
-        std::swap(tmp.at(k++), arr->at(j++));
-    }
+            while (i <= mid) {
+                std::swap(tmp.at(k++), arr->at(i++));
+            }
+            while (j <= high) {
+                std::swap(tmp.at(k++), arr->at(j++));
+            }
+            for (i = low, k = 0; i <= high; i++, k++) {
+                std::swap(tmp.at(k), arr->at(i));
+            }
 
-    while (i <= mid) {
-      std::swap(tmp.at(k++), arr->at(i++));
-    }
-    while (j <= high) {
-      std::swap(tmp.at(k++), arr->at(j++));
-    }
-    for (i = low, k = 0; i <= high; i++, k++) {
-      std::swap(tmp.at(k), arr->at(i));
-    }
+            std::vector<SortObject<T>>().swap(tmp);
+        }
 
-    std::vector<SortObject<T>>().swap(tmp);
-    }
+        void mergesort(std::vector<SortObject<T>> *arr, int low, int high) {
+            int mid;
+            if (low < high) {
+                mid = (low + high) / 2;
+                // Left hand side.
+                mergesort(arr, low, mid);
+                // Right hand side.
+                mergesort(arr, mid + 1, high);
+                Merge(arr, low, mid, high);
+            }
+        }
 
-    void mergesort(std::vector<SortObject<T>> *arr, int low, int high) {
-      int mid;
-      if (low < high) {
-      mid = (low + high) / 2;
-      // Left hand side.
-      mergesort(arr, low, mid);
-      // Right hand side.
-      mergesort(arr, mid + 1, high);
-      Merge(arr, low, mid, high);
-      }
-    }
-
-    void doSort(std::vector<SortObject<T>> *reformedArray) override {
-    mergesort(reformedArray, 0, reformedArray->size() - 1);
-    }
+        void doSort(std::vector<SortObject<T>> *reformedArray) override {
+            mergesort(reformedArray, 0, reformedArray->size() - 1);
+        }
 };
 }   // namespace llama
 #endif   // LLAMA_CPP_SRC_SORT_MERGESORT_H_
