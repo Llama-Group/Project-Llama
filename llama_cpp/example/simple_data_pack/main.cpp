cec3550e40290bcdbe61ee3e2f56868b68dcb769
神楽坂碧玺 <0xbbc@0xbbc.com>
2016-09-30 20:06:04 +0800

    Adjust example

--- llama_cpp/example/simple_data_pack/main.cpp
+++ llama_cpp/example/simple_data_pack/main.cpp
@@ -1,25 +1,16 @@
 //
-//  Copyright © 2016 Project Llama. All rights reserved.
+//  main.cpp
+//  SimpleDataPack
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+//  Created by BlueCocoa on 2016/9/30.
+//  Copyright © 2016 BlueCocoa. All rights reserved.
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-//
-
-#include <Utility/SimpleDataPack.h>
 
 #include <iostream>
+#include "SimpleDataPack.h"
 
 class SimpleObject {
-public:
+ public:
     int data;
 };
 
@@ -32,7 +23,7 @@ int main(int argc, const char * argv[]) {
     }
     
     for (int i = 0; i < 4; i++) {
-        unsigned short int16 = UINT16_MAX - i;
+        int16_t int16 = UINT16_MAX - i;
         pack.push(SimpleDataPack::data_t::BIT_16, "int16", &int16, false);
     }
     
@@ -41,7 +32,7 @@ int main(int argc, const char * argv[]) {
     }
     
     for (int i = 0; i < 4; i++) {
-        long long int64 = INT64_MAX - i;
+        int64_t int64 = INT64_MAX - i;
         pack.push(SimpleDataPack::data_t::BIT_64, "int64", &int64);
     }
     
@@ -50,7 +41,7 @@ int main(int argc, const char * argv[]) {
         pack.push(SimpleDataPack::data_t::BIT_128, "long double", &p);
     }
     
-    pack.push_pure_data("cstring", (void *)"Hello\0", 6);
+    pack.push_pure_data("cstring", reinterpret_cast<void *>("Hello\0"), 6);
     
     for (int i = 0; i < 4; i++) {
         SimpleObject object;
@@ -58,42 +49,43 @@ int main(int argc, const char * argv[]) {
         pack.push(SimpleDataPack::data_t::SIMPLE_OBJECT, "object", &object);
     }
     
-    pack.saveTo("233.dat");
+    pack.saveTo("/Users/BlueCocoa/Desktop/233.dat");
     
-    SimpleDataPack::loadFrom("233.dat", [](unsigned short data_type, std::string id, void * data, size_t length, bool _signed) -> bool {
+    SimpleDataPack::loadFrom("/Users/BlueCocoa/Desktop/233.dat", [](
+        uint16_t data_type, std::string id, void * data, size_t length, bool _signed) -> bool {
         switch (data_type) {
             case SimpleDataPack::data_t::BIT_8:
                 std::cout << "BIT_8:\n";
                 std::cout << "  id: " << id << '\n';
-                std::cout << "  data: " << (int)(*(reinterpret_cast<char *>(data))) << '\n';
+                std::cout << "  data: " << static_cast<int>(*(reinterpret_cast<char *>(data))) << '\n';
                 std::cout << "  length: " << length << '\n';
                 std::cout << "  _signed: " << (_signed ? "YES" : "NO") << '\n';
                 break;
             case SimpleDataPack::data_t::BIT_16:
                 std::cout << "BIT_16:\n";
                 std::cout << "  id: " << id << '\n';
-                std::cout << "  data: " << (unsigned short)(*(reinterpret_cast<unsigned short *>(data))) << '\n';
+                std::cout << "  data: " << static_cast<uint16_t>(*(reinterpret_cast<uint16_t *>(data))) << '\n';
                 std::cout << "  length: " << length << '\n';
                 std::cout << "  _signed: " << (_signed ? "YES" : "NO") << '\n';
                 break;
             case SimpleDataPack::data_t::BIT_32:
                 std::cout << "BIT_32:\n";
                 std::cout << "  id: " << id << '\n';
-                std::cout << "  data: " << (int)(*(reinterpret_cast<int *>(data))) << '\n';
+                std::cout << "  data: " << static_cast<int>(*(reinterpret_cast<int *>(data))) << '\n';
                 std::cout << "  length: " << length << '\n';
                 std::cout << "  _signed: " << (_signed ? "YES" : "NO") << '\n';
                 break;
             case SimpleDataPack::data_t::BIT_64:
                 std::cout << "BIT_64:\n";
                 std::cout << "  id: " << id << '\n';
-                std::cout << "  data: " << (int64_t)(*(reinterpret_cast<int64_t *>(data))) << '\n';
+                std::cout << "  data: " << static_cast<int64_t>(*(reinterpret_cast<int64_t *>(data))) << '\n';
                 std::cout << "  length: " << length << '\n';
                 std::cout << "  _signed: " << (_signed ? "YES" : "NO") << '\n';
                 break;
             case SimpleDataPack::data_t::BIT_128:
                 std::cout << "BIT_128:\n";
                 std::cout << "  id: " << id << '\n';
-                std::cout << "  data: " << (long double)(*(reinterpret_cast<long double *>(data))) << '\n';
+                std::cout << "  data: " << static_cast<long double>(*(reinterpret_cast<long double *>(data))) << '\n';
                 std::cout << "  length: " << length << '\n';
                 std::cout << "  _signed: " << (_signed ? "YES" : "NO") << '\n';
                 break;
